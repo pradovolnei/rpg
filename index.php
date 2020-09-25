7959992fd0bfe9b20a2ab836d0e2d3bf4436e907
pradovolnei <prado.volnei@gmail.com>
2020-09-25 11:43:50 -0300

    inserindo layout e personagens

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php 
-	
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>RPG</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="dist/css/cenario.css">
+</head>
+<body>
+	<div id="cenario">
+		<div id="fundo" > <img src="dist/img/bg.jpg" /> </div>
+		<div id="personagem" > <img src="dist/img/guerreiro.png" width="30%" /> </div>
+		<div id="inimigo" > <img src="dist/img/orc.png"  /> </div>
+	</div>
+</body>
+
+</html>
